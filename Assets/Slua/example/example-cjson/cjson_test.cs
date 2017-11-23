2ed74581f9ecc75765e0a80c60ce948d46a4570f
noeticwxb <noetic_wxb@126.com>
2017-11-23 20:08:51 +0800

    add test example

---
+++ Assets/Slua/example/example-cjson/cjson_test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SLua;
+using System;
+
+
+public class cjson_test : MonoBehaviour
+{
+	LuaSvr l;
+
+
+	void Start()
+	{
+		l = new LuaSvr();
+		l.init(null,()=>{l.start("cjsonTest");});
+	}
+
+	void Update()
+	{
+
+	}
+}

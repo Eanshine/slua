1dc313230d75ecbc60aa95195dbcf3636d37e19e
noeticwxb <noetic_wxb@126.com>
2017-11-27 17:53:47 +0800

    compile for mac

--- build/make_osx_jit.sh
+++ build/make_osx_jit.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-cp slua.c luasocket-mini/* lua-cjson-2.1.0/* luajit-2.1.0-beta3/src/
 cd luajit-2.1.0-beta3
 
+git clean -f
 make clean
 make CC="gcc -m32"
 cp src/luajit ../../jit/mac/x86/luajit
 
-
+git clean -f
 make clean
 make CC="gcc"
 cp src/luajit ../../jit/mac/x64/luajit
 
-
+git clean -f
 make clean
 make CC="gcc" CFLAGS=-DLUAJIT_ENABLE_GC64
 cp src/luajit ../../jit/mac/gc64/luajit
 
-
+git clean -f
 make clean
 make CC="gcc -m32" BUILDMODE=static
 cp src/libluajit.a ../luajit-osx/libluajit_x86.a
 
+git clean -f
 make clean
 make CC="gcc" BUILDMODE=static CFLAGS=-DLUAJIT_ENABLE_GC64
 cp src/libluajit.a ../luajit-osx/libluajit_x86_64.a
+
+make clean
+make clean -f
+
 
 cd ../luajit-osx/
 xcodebuild -configuration=Release

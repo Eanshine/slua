d228c5abd1f65110b63a3eb8ac7a3439adb72eec
noeticwxb <noetic_wxb@126.com>
2020-06-11 22:42:04 +0800

    add sproto test

---
+++ Assets/Slua/example/example-sproto/sproto_Test.cs
@@ -0,0 +1,28 @@
+﻿using SLua;
+using UnityEngine;
+
+public class sproto_Test : MonoBehaviour
+{
+
+    LuaSvr l;
+
+    // Use this for initialization
+    void Start()
+    {
+        l = new LuaSvr();
+        l.init(null, () =>
+        {
+            Debug.Log("---------sproto Test-----------");
+            l.start("sprotoTest");
+
+        },
+
+        LuaSvrFlag.LSF_BASIC | LuaSvrFlag.LSF_EXTLIB | LuaSvrFlag.LSF_3RDDLL);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
